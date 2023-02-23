e4b71a9b4afc9433db590812f16857dbcd5b393a
Matheus Moraes <mateyc_2013@outlook.com>
2023-02-23 14:38:56 -0300

    Submit button

--- Atividade_01/main.js
+++ Atividade_01/main.js
@@ -6,8 +6,9 @@ window.onload = function load()
     
     for (let index = current_step + 1; index <= number_steps; index++)
     {
-        console.log(index);
         document.getElementById("passo-" + index).style.display = "none";
+        document.getElementById("submit").style.display = "none"
+        document.getElementById("prev").setAttribute("disabled","disabled");
     }
     
         
@@ -17,6 +18,11 @@ function previous_step()
 {
     if (current_step > 1) 
     {
+        
+        document.getElementById("submit").style.display = "none";
+        document.getElementById("next").style.display = "inline";
+
+        document.getElementById("next").innerHTML = "Próximo passo";
         document.getElementById("passo-"+ current_step).style.display = "none";
         document.getElementById("mark-"+current_step).classList.remove('green');
         document.getElementById("mark-"+current_step).classList.add('gray');
@@ -26,7 +32,12 @@ function previous_step()
         document.getElementById("mark-" + current_step).classList.remove('gray');
         document.getElementById("mark-" + current_step).classList.add('green');
     }
-    
+
+    if (current_step == 1) 
+    {
+        document.getElementById("prev").setAttribute("disabled","disabled");
+    }
+
 }
 
 
@@ -41,8 +52,14 @@ function next_step()
     
         document.getElementById("passo-" + current_step).style.display = "block";
         document.getElementById("mark-" + current_step).classList.remove('gray');
-        document.getElementById("mark-" + current_step).classList.add('green'); 
+        document.getElementById("mark-" + current_step).classList.add('green');
+        document.getElementById("prev").removeAttribute("disabled", "disabled");
     }
 
+    if (current_step == number_steps) 
+    {
+        document.getElementById("next").style.display = "none";
+        document.getElementById("submit").style.display = "inline";
+    }
     
 }
