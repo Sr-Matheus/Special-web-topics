6a359c17958cc85f5885438860e2ff5e08020ddc
Matheus Moraes <mateyc_2013@outlook.com>
2023-04-20 21:53:45 -0300

    Save modifies in array

--- Atividade_AV2/index.js
+++ Atividade_AV2/index.js
@@ -2,13 +2,37 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const expbhs = require('express-handlebars');
-
-app.engine('handlebars', expbhs.engine());
-app.set('view engine', 'handlebars');
+const hbs = expbhs.create({partialsDir:["views/patials"]});
 
 app.use(express.static('public'));
 app.use(express.static('resources'));
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', expbhs.engine());
+app.set('view engine', 'handlebars');
+
+
+var tasks =
+[
+    
+]; 
+
+app.post('/app/add',(req,res)=>
+{
+    const task = req.body.task;
+    
+    tasks.push({task});
+
+    console.log(tasks); 
+    
+    res.render('home-added', {tasks});
+});
+
+
+
+
+
+
 
 app.get('/', (req,res) =>
 {
@@ -21,12 +45,6 @@ app.get('/app', (req,res) =>
 });
 
 
-app.post('/app/add',(req,res)=>
-{
-    const task = req.body.task;
-    
-    res.render('home-added', { task:task });
-});
 
 app.use(function (req,res){
     res.status(404).render('404');
